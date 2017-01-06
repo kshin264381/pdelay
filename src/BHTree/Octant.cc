9b272ff55a71163d0777c6ef809ee8a4f667507a
kshin264381 <kshin@aps.anl.gov>
2017-01-06 16:34:31 -0600

    No more inefficiency on Octant.cc
    Ok, solved the hanging problem by removing some useless stuff at Octant
    generator routine. But it seems force calculation is a bit off. Let's
    see what happens.

--- src/BHTree/Octant.cc
+++ src/BHTree/Octant.cc
@@ -43,87 +43,86 @@ std::shared_ptr<Octant> Octant::NewOctant(const spCarrier& some_carrier)
 {
     // Determine some_carrier's location from
     // center of this Octant.
-    auto rel_loc = \
-        std::make_shared<Loc>(some_carrier->GetPos() - this->center);
+    auto rel_loc = some_carrier->GetPos() - this->center;
 
     // determining relative pos
     // --> True: positive side,
     // --> False: negative side,
     //
     bool x_rel, y_rel, z_rel;
-    x_rel = fp_lt<fp_t>(rel_loc->x, FP_T(0.0));
-    y_rel = fp_lt<fp_t>(rel_loc->y, FP_T(0.0));
-    z_rel = fp_lt<fp_t>(rel_loc->z, FP_T(0.0));
+    x_rel = fp_lt<fp_t>(rel_loc.x, FP_T(0.0));
+    y_rel = fp_lt<fp_t>(rel_loc.y, FP_T(0.0));
+    z_rel = fp_lt<fp_t>(rel_loc.z, FP_T(0.0));
 
-    auto new_len = std::make_shared<Dim>(this->length / 2.0);
-    auto new_len_adj = std::make_shared<Dim>(*new_len / 4.0);
-    std::shared_ptr<Loc> new_center = nullptr;
+    auto new_len = this->length / 2.0;
+    auto new_len_adj = new_len / 4.0;
+    auto new_center = ZeroLoc;
 
     // Now, deal with 8 cases.
     //
     // x_rel > 0, y_rel > 0, z_rel > 0 111
     if (x_rel && y_rel && z_rel) {
-        new_center = \
-            std::make_shared<Loc>(
-            center.x + new_len_adj->x,
-                center.y + new_len_adj->y,
-                center.z + new_len_adj->z);
+        new_center = Loc{
+            center.x + new_len_adj.x,
+            center.y + new_len_adj.y,
+            center.z + new_len_adj.z
+        };
     }
     // x_rel > 0, y_rel > 0, z_rel < 0 110
     if (x_rel && y_rel && !z_rel) {
-        new_center = \
-            std::make_shared<Loc>(
-            center.x + new_len_adj->x,
-                center.y + new_len_adj->y,
-                center.z - new_len_adj->z );
+        new_center = Loc{
+            center.x + new_len_adj.x,
+            center.y + new_len_adj.y,
+            center.z - new_len_adj.z
+        };
     }
     // x_rel > 0, y_rel < 0, z_rel > 0 101
     if (x_rel && !y_rel && z_rel) {
-        new_center = \
-            std::make_shared<Loc>(
-            center.x + new_len_adj->x,
-                center.y - new_len_adj->y,
-                center.z + new_len_adj->z);
+        new_center = Loc{
+            center.x + new_len_adj.x,
+            center.y - new_len_adj.y,
+            center.z + new_len_adj.z
+        };
     }
     // x_rel > 0, y_rel < 0, z_rel < 0 100
     if (x_rel && !y_rel && !z_rel) {
-        new_center = \
-            std::make_shared<Loc>(
-            center.x + new_len_adj->x,
-                center.y - new_len_adj->y,
-                center.z - new_len_adj->z);
+        new_center = Loc{
+            center.x + new_len_adj.x,
+            center.y - new_len_adj.y,
+            center.z - new_len_adj.z
+        };
     }
     // x_rel < 0, y_rel > 0, z_rel > 0 011
     if (!x_rel && y_rel && z_rel) {
-        new_center = \
-            std::make_shared<Loc>(
-            center.x - new_len_adj->x,
-                center.y + new_len_adj->y,
-                center.z + new_len_adj->z);
+        new_center = Loc{
+            center.x - new_len_adj.x,
+            center.y + new_len_adj.y,
+            center.z + new_len_adj.z
+        };
     }
     // x_rel < 0, y_rel < 0, z_rel > 0 001
     if (!x_rel && !y_rel && z_rel) {
-        new_center = \
-            std::make_shared<Loc>(
-            center.x - new_len_adj->x,
-                center.y - new_len_adj->y,
-                center.z + new_len_adj->z);
+        new_center = Loc{
+            center.x - new_len_adj.x,
+            center.y - new_len_adj.y,
+            center.z + new_len_adj.z
+        };
     }
     // x_rel < 0, y_rel > 0, z_rel < 0 010
     if (!x_rel && y_rel && !z_rel) {
-        new_center = \
-            std::make_shared<Loc>(
-            center.x - new_len_adj->x,
-                center.y + new_len_adj->y,
-                center.z - new_len_adj->z);
+        new_center = Loc{
+            center.x - new_len_adj.x,
+            center.y + new_len_adj.y,
+            center.z - new_len_adj.z
+        };
     }
     // x_rel < 0, y_rel < 0, z_rel < 0 000
     if (!x_rel && !y_rel && !z_rel) {
-        new_center = \
-            std::make_shared<Loc>(
-            center.x - new_len_adj->x,
-                center.y - new_len_adj->y,
-                center.z - new_len_adj->z);
+        new_center = Loc{
+            center.x - new_len_adj.x,
+            center.y - new_len_adj.y,
+            center.z - new_len_adj.z
+        };
     }
 
     return std::make_shared<Octant>(new_len, new_center);
@@ -134,17 +133,16 @@ int Octant::GetOctantPart(const spCarrier& carrier) const
 {
     // Determine some_carrier's location from
     // center of this Octant.
-    auto rel_loc = \
-        std::make_unique<Loc>(carrier->GetPos() - this->center);
+    auto rel_loc = carrier->GetPos() - this->center;
 
     // determining relative pos
     // --> True: positive side,
     // --> False: negative side,
     //
     bool x_rel, y_rel, z_rel;
-    x_rel = fp_lt<fp_t>(rel_loc->x, FP_T(0.0));
-    y_rel = fp_lt<fp_t>(rel_loc->y, FP_T(0.0));
-    z_rel = fp_lt<fp_t>(rel_loc->z, FP_T(0.0));
+    x_rel = fp_lt<fp_t>(rel_loc.x, FP_T(0.0));
+    y_rel = fp_lt<fp_t>(rel_loc.y, FP_T(0.0));
+    z_rel = fp_lt<fp_t>(rel_loc.z, FP_T(0.0));
 
     // uNE, uNW, uSE, uSW, lNE, lNW, lSE, lSW
     // are mapped to...
@@ -187,82 +185,90 @@ int Octant::GetOctantPart(const spCarrier& carrier) const
 // Generate octant by force.
 std::shared_ptr<Octant> Octant::uNE() const // 111
 {
-    auto new_len = std::make_shared<Dim>(this->length / 2.0);
-    auto new_len_adj = std::make_shared<Dim>(*new_len / 4.0);
-    auto new_center = std::make_shared<Loc>(
-        center.x + new_len_adj->x,
-        center.y + new_len_adj->y,
-        center.z + new_len_adj->z);
+    auto new_len = this->length / 2.0;
+    auto new_len_adj = new_len / 4.0;
+    auto new_center = Loc{
+        center.x + new_len_adj.x,
+        center.y + new_len_adj.y,
+        center.z + new_len_adj.z
+    };
     return std::make_shared<Octant>(new_len, new_center);
 }
 std::shared_ptr<Octant> Octant::uNW() const // 011
 {
-    auto new_len = std::make_shared<Dim>(this->length / 2.0);
-    auto new_len_adj = std::make_shared<Dim>(*new_len / 4.0);
-    auto new_center = std::make_shared<Loc>(
-        center.x - new_len_adj->x,
-        center.y + new_len_adj->y,
-        center.z + new_len_adj->z);
+    auto new_len = this->length / 2.0;
+    auto new_len_adj = new_len / 4.0;
+    auto new_center = Loc{
+        center.x - new_len_adj.x,
+        center.y + new_len_adj.y,
+        center.z + new_len_adj.z
+    };
     return std::make_shared<Octant>(new_len, new_center);
 }
 std::shared_ptr<Octant> Octant::uSE() const // 101
 {
-    auto new_len = std::make_shared<Dim>(this->length / 2.0);
-    auto new_len_adj = std::make_shared<Dim>(*new_len / 4.0);
-    auto new_center = std::make_shared<Loc>(
-        center.x + new_len_adj->x,
-        center.y - new_len_adj->y,
-        center.z + new_len_adj->z);
+    auto new_len = this->length / 2.0;
+    auto new_len_adj = new_len / 4.0;
+    auto new_center = Loc{
+        center.x + new_len_adj.x,
+        center.y - new_len_adj.y,
+        center.z + new_len_adj.z
+    };
     return std::make_shared<Octant>(new_len, new_center);
 }
 std::shared_ptr<Octant> Octant::uSW() const // 001
 {
-    auto new_len = std::make_shared<Dim>(this->length / 2.0);
-    auto new_len_adj = std::make_shared<Dim>(*new_len / 4.0);
-    auto new_center = std::make_shared<Loc>(
-        center.x - new_len_adj->x,
-        center.y - new_len_adj->y,
-        center.z + new_len_adj->z);
+    auto new_len = this->length / 2.0;
+    auto new_len_adj = new_len / 4.0;
+    auto new_center = Loc{
+        center.x - new_len_adj.x,
+        center.y - new_len_adj.y,
+        center.z + new_len_adj.z
+    };
     return std::make_shared<Octant>(new_len, new_center);
 }
 std::shared_ptr<Octant> Octant::lNE() const // 110
 {
-    auto new_len = std::make_shared<Dim>(this->length / 2.0);
-    auto new_len_adj = std::make_shared<Dim>(*new_len / 4.0);
-    auto new_center = std::make_shared<Loc>(
-        center.x + new_len_adj->x,
-        center.y + new_len_adj->y,
-        center.z - new_len_adj->z);
+    auto new_len = this->length / 2.0;
+    auto new_len_adj = new_len / 4.0;
+    auto new_center = Loc{
+        center.x + new_len_adj.x,
+        center.y + new_len_adj.y,
+        center.z - new_len_adj.z
+    };
     return std::make_shared<Octant>(new_len, new_center);
 }
 std::shared_ptr<Octant> Octant::lNW() const // 010
 {
-    auto new_len = std::make_shared<Dim>(this->length / 2.0);
-    auto new_len_adj = std::make_shared<Dim>(*new_len / 4.0);
-    auto new_center = std::make_shared<Loc>(
-        center.x - new_len_adj->x,
-        center.y + new_len_adj->y,
-        center.z - new_len_adj->z);
+    auto new_len = this->length / 2.0;
+    auto new_len_adj = new_len / 4.0;
+    auto new_center = Loc{
+        center.x - new_len_adj.x,
+        center.y + new_len_adj.y,
+        center.z - new_len_adj.z
+    };
     return std::make_shared<Octant>(new_len, new_center);
 }
 std::shared_ptr<Octant> Octant::lSE() const // 100
 {
-    auto new_len = std::make_shared<Dim>(this->length / 2.0);
-    auto new_len_adj = std::make_shared<Dim>(*new_len / 4.0);
-    auto new_center = std::make_shared<Loc>(
-        center.x + new_len_adj->x,
-        center.y - new_len_adj->y,
-        center.z - new_len_adj->z);
+    auto new_len = this->length / 2.0;
+    auto new_len_adj = new_len / 4.0;
+    auto new_center = Loc{
+        center.x + new_len_adj.x,
+        center.y - new_len_adj.y,
+        center.z - new_len_adj.z
+    };
     return std::make_shared<Octant>(new_len, new_center);
 }
 std::shared_ptr<Octant> Octant::lSW() const // 000
 {
-    auto new_len = std::make_shared<Dim>(this->length / 2.0);
-    auto new_len_adj = std::make_shared<Dim>(*new_len / 4.0);
-    auto new_center = std::make_shared<Loc>(
-        center.x - new_len_adj->x,
-        center.y - new_len_adj->y,
-        center.z - new_len_adj->z);
+    auto new_len = this->length / 2.0;
+    auto new_len_adj = new_len / 4.0;
+    auto new_center = Loc{
+        center.x - new_len_adj.x,
+        center.y - new_len_adj.y,
+        center.z - new_len_adj.z
+    };
     return std::make_shared<Octant>(new_len, new_center);
 }
 
